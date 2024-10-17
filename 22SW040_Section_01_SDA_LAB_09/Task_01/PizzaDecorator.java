bc599caa5d5e8e8620ba420816a4f72fa4a1f851
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-17 05:12:41 -0700

    Create PizzaDecorator.java

---
+++ 22SW040_Section_01_SDA_LAB_09/Task_01/PizzaDecorator.java
@@ -0,0 +1,16 @@
+// Creating  Cheese topping class
+class Cheese extends PizzaDecorator {
+    public Cheese(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public String getDescription() {
+        return pizza.getDescription() + ", Cheese";
+    }
+
+    @Override
+    public double getCost() {
+        return pizza.getCost() + 50.0;
+    }
+}
