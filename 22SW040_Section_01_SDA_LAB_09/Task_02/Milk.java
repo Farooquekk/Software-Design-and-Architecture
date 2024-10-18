c0bb63aaf1b06c508048ccd02c12ef98708598aa
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-18 05:11:51 -0700

    Create Milk.java

---
+++ 22SW040_Section_01_SDA_LAB_09/Task_02/Milk.java
@@ -0,0 +1,16 @@
+// Creating class Milk as a condiment
+class Milk extends CoffeeDecorator {
+    public Milk(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 20.0;
+    }
+}
