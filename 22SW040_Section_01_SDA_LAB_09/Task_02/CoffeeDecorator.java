878012e3313371d438ca45a0fbaf2d3e55d92cb3
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-18 05:09:49 -0700

    Create CoffeeDecorator.java

---
+++ 22SW040_Section_01_SDA_LAB_09/Task_02/CoffeeDecorator.java
@@ -0,0 +1,19 @@
+// Creating abstract CoffeeDecorator class
+
+abstract class CoffeeDecorator implements Coffee {
+    protected Coffee coffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost();
+    }
+}
